5fd8daeed176688ecad421754e0eb51422629ccc
Zhongyue Luo <lzyeval@gmail.com>
2012-02-16 22:05:31 +0800

    fix protocol

--- demux/common/protocol.py
+++ demux/common/protocol.py
@@ -1,47 +1,50 @@
+#!/usr/bin/env python
 
+"""
 msg_type = 'lb'
 msg_id = str(uuid.uuid1())
 
 ======== client -> server ==============
 
-# create_lb
-msg_body = {'cmd': 'create_lb',   
+# 'create_lb'
+msg_body = {'cmd': 'create_lb',
             'msg': {'user_id': 'myUser',
                     'project_id': 'myProject',
                     'load_balancer_id': 'myLB',
-                    'protocol': random.choice(['http', 'tcp']),
+                    # http, tcp
+                    'protocol': http,
                     'listen_port': 80,
                     'instance_port': 80,
-                    'balancing_method': random.choice(['a_meth', 'round_robin']),
-                    'health_check': {'timeout_ms': 5,
-                                     'interval_ms': 0.5
-                                     # if http
-                                     'target_path': '/'
-                                     'fail_count': 2
-                                     # elif tcp
-                                     'healthy_treshold':10,
-                                     'unhealthy_treshold':2,
-                                    },
-                    'instance_ids': [23, 44, 82]
-                    'http_server_names': ['www.abc.com', 'www.xyz.com']
+                    # round_robin, source_binding
+                    'balancing_method': 'round_robin',
+                    'health_check_timeout_ms': 5,
+                    'health_check_interval_ms': 0.5,
+                    # if http
+                    'health_check_target_path': '/',
+                    'health_check_fail_count': 2,
+                    # elif tcp
+                    'health_check_healthy_treshold':10,
+                    'health_check_unhealthy_treshold':2,
+                    # end if
+                    'instance_ids': [23, 44, 82],
+                    'http_server_names': ['www.abc.com', 'www.xyz.com'],
                    }
            }
 
-# delete or 'read_lb'
-message = {'cmd': random.choice(['delete_lb','read_lb'])
-           'msg': {'user_id': 'myUser'
-                   'project_id': 'myLB'
+# 'delete_lb' or 'read_lb'
+message = {'cmd': 'delete_lb',
+           'msg': {'user_id': 'myUser',
+                   'project_id': 'myLB',
                    'load_balancer_id': 'myLB',
-                   }
-           }
+                  }
+          }
 
-# 'read_all_lb'
+# 'read_lb_list'
 message = {'cmd': 'read_all_lb',
            'msg': {'user_id': 'myUser'
                    'project_id': 'myLB'
-                   }
-           }
-# update
+                  }
+          }
 
 # After you create a load balancer, you can modify any of the settings,
 # except for Load Balancer Name and Port Configuration.
@@ -49,70 +52,123 @@ message = {'cmd': 'read_all_lb',
 # To rename a load balancer or change its port configuration,
 # create a replacement load balancer.
 
-# update 一个或多个字段
+# 'update_lb_metadata'
 message = {'cmd': 'update_lb_metadata',   
            'msg': {'user_id': 'myUser',
                    'project_id': 'myProject',
                    'load_balancer_id': 'myLB',
-                   'balancing_method': random.choice(['a_meth', 'round_robin']),
-                   'health_check': {'timeout_ms': 5,
-                                    'interval_ms': 0.5
-                                    # if http
-                                    'target_path': '/'
-                                    'fail_count': 2
-                                    # elif tcp
-                                    'healthy_treshold':10,
-                                    'unhealthy_treshold':2,
-                                    },
-                   }
-           }
+                   # round_robin, source_binding
+                   'balancing_method': 'round_robin',
+                   'health_check_timeout_ms': 5,
+                   'health_check_interval_ms': 0.5,
+                   # if http
+                   'health_check_target_path': '/',
+                   'health_check_fail_count': 2,
+                   # elif tcp
+                   'health_check_healthy_treshold':10,
+                   'health_check_unhealthy_treshold':2,
+                  }
+          }
 
-# update instances
+# 'update_lb_instance_ids'
 message = {'cmd': 'update_lb_instance_ids',   
            'msg': {'user_id': 12,
                    'project_id': 'myProject',
                    'load_balancer_id': 'myLB',
-                   'instance_ids': [id1, id2]
-                   }
-           }
+                   'instance_ids': [23, 44, 82],
+                  }
+          }
 
-# update http_server_names
-message = {'cmd': 'update_lb_instance_ids',   
+# 'update_lb_http_server_names'
+message = {'cmd': 'update_lb_http_server_names',
            'msg': {'user_id': 12,
                    'project_id': 'myProject',
                    'load_balancer_id': 'myLB',
-                   'http_server_names': ['www.abc.com', 'www.xyz.com']
-                   }
-           }
+                   'http_server_names': ['www.abc.com', 'www.xyz.com'],
+                  }
+          }
 
 ======== server -> client ==============
 
-======== server -> worker ==============
+# 'create_lb', 'update_lb', 'delete_lb'
+to_client = {'cmd': <cmd>,
+             'msg' {'desc': 'message string'
+                    # 200 or 500
+                    'code': 200,
+                   }
+            }
 
-to_worker = {'cmd': random.choice('create_lb', 'update_lb'),
+# 'read_lb'
+to_client = {'cmd': <cmd>,
              'msg': {'user_id': 'myUser',
                      'project_id': 'myProject',
                      'load_balancer_id': 'myLB',
-                     'protocol': random.choice(['http', 'tcp']),
-                     'listen_port': 80,
-                     'instance_port': 80,
-                     'balancing_method': random.choice(['a_meth', 'round_robin']),
-                     'health_check': {'timeout_ms': 5,
-                                      'interval_ms': 0.5
-                                      # if http
-                                      'target_path': '/'
-                                      'fail_count': 2
-                                      # elif tcp
-                                      'healthy_treshold':10,
-                                      'unhealthy_treshold':2,
-                                     },
-                     'instance_ids': [23, 44, 82]
-                     # add instance fixed ips and send to worker
-                     'instance_ips': ['10.4.5.6', '10.3.4.5', '10.8.8.8']
-                     'http_server_names': ['www.abc.com', 'www.xyz.com']
+                     # round_robin, source_binding
+                     'balancing_method': 'round_robin',
+                     'health_check_timeout_ms': 5,
+                     'health_check_interval_ms': 0.5,
+                     # if http
+                     'health_check_target_path': '/',
+                     'health_check_fail_count': 2,
+                     # elif tcp
+                     'health_check_healthy_treshold':10,
+                     'health_check_unhealthy_treshold':2,
+                     # end if
+                     'instance_ids': [23, 44, 82],
+                     'http_server_names': ['www.abc.com', 'www.xyz.com'],
                     }
             }
 
+# 'read_lb_list'
+to_client = {'cmd': <cmd>,
+             'msg': {'user_id': 'myUser',
+                     'project_id': 'myProject',
+                     'load_balancer_list': [{'load_balancer_id': 'myLB',
+                                             # http, tcp
+                                             'protocol': http,
+                                             'listen_port': 80,
+                                             'instance_port': 80,
+                                            },
+                                            {'load_balancer_id': 'myLB',
+                                             # http, tcp
+                                             'protocol': http,
+                                             'listen_port': 80,
+                                             'instance_port': 80,
+                                            },
+                                           ],
+                    }
+            }
+
+======== server -> worker ==============
+
+# 'create_lb', 'update_lb' -> all data of lb must be sent to worker
+to_worker = {'cmd': 'create_lb',
+             'msg': {'user_id': 'myUser',
+                     'project_id': 'myProject',
+                     'load_balancer_id': 'myLB',
+                     # http, tcp
+                     'protocol': 'http',
+                     'listen_port': 80,
+                     'instance_port': 80,
+                     # round_robin, source_binding
+                     'balancing_method': 'round_robin',
+                     'health_check_timeout_ms': 5,
+                     'health_check_interval_ms': 0.5,
+                     # if http
+                     'health_check_target_path': '/',
+                     'health_check_fail_count': 2,
+                     # elif tcp
+                     'health_check_healthy_treshold':10,
+                     'health_check_unhealthy_treshold':2,
+                     # end if
+                     'instance_ids': [23, 44, 82],
+                     # add instance fixed ips and send to worker
+                     'instance_ips': ['10.4.5.6', '10.3.4.5', '10.8.8.8']
+                     'http_server_names': ['www.abc.com', 'www.xyz.com'],
+                    }
+            }
+
+# 'delete_lb'
 to_worker= {'cmd': 'delete_lb',
             'msg': {'user_id': 'myUser'
                     'project_id': 'myLB'
@@ -123,7 +179,10 @@ to_worker= {'cmd': 'delete_lb',
 ======== worker -> server ==============
 
 from_worker = {'cmd': <cmd>,
-               'msg' {'code': 200,
-                      'desc': 'sdkf;askjdgfa'
+               'msg' {'worker_id': 2,
+                      # 200 or 500
+                      'code': 200,
+                      'desc': 'message string'
                      }
               }
+"""
--- demux/server/db.py
+++ demux/server/db.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import MySQLdb
+
+insert_lb = ("INSERT INTO load_balancer (id, protocol, listen_port, "
+                                        "balanceing_method, "
+                                        "health_checkout_timeout_ms, "
+                                        "health_check_interval_ms, "
+                                        "health_checkout_target_path, "
+                                        "health_check_fail_count, "
+                                        "health_checkout_health_threshold, "
+                                        "health_check_unhealthy_threshold) "
+             "VALUES (%(load_balancer_id) ")
+
 def create_lb(user_id, project_id, lb_id, **kwargs):
     pass
 
